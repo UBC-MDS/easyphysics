3ee0e3bf8ab8e73476a51a9de60f0ff12ff1a97b
Revathy Ponnambalam <revathyp@student.ubc.ca>
2023-01-13 23:52:19 -0800

    updated function gravitational_energy

---
+++ src/easyphysics/gravitational_energy.py
@@ -0,0 +1,25 @@
+# def gravitational_energy(mass, g = 9.8,height):
+#     """ calculate the energy possessed or acquired by an object due to a change in its position when it is present #       in a gravitational field  = m*g*h """
+
+#     Parameters
+#     ----------
+#     m : float
+#         A numeric value that is the mass of the object(kilograms).
+#     g : float, optional
+#        gravitational field 
+#     h: float
+#        height above the earth surface(metre).
+
+#     Returns
+#     -------
+#     GE : float
+#         A gravitational energy (float) value which is the energy stored in the object.
+
+#     Examples
+#     --------
+#     >>> gravitational_energy(2.5, g = 9.8,10)
+#     245.16
+#     >>> gravitational_energy(10, g = 9.80665, 22)
+#     2157.463 
+#     """
+#    return GE 
