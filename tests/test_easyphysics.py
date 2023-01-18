6b95ec428aee9cf40672c2dc0c282641d3af49ad
Nikita Susan Easow <nik11susan@gmail.com>
2023-01-17 17:07:12 -0800

    added test cases for kinetic energy + changed toml file

--- tests/test_easyphysics.py
+++ tests/test_easyphysics.py
@@ -1,1 +1,11 @@
-from easyphysics import easyphysics
+import sys
+#sys.path.append("..\\src\\")
+print(sys.path)
+from src.easyphysics.kinetic_energy import kinetic_energy 
+
+def test_kinetic_energy():
+    m = 10
+    v = 2.3
+    expected = 26.45
+    actual = kinetic_energy(m, v)
+    assert actual == expected, "Kinetic Energy is wrong! Check again!"
